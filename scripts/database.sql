10f08326d91094d6dc2297fee767ebddd12a3024
root <keya5541@colorado.edu>
2017-11-03 13:44:55 -0600

    added milestone 3

---
+++ scripts/database.sql
@@ -0,0 +1,167 @@
+CREATE DATABASE cafe;
+USE cafe;
+CREATE TABLE users 
+(
+    userID int,
+    firstName varchar(255),
+    lastName varchar(255),
+    dateOfBirth int,
+    email varchar(255),
+    passW varchar(255),
+    phone int,
+    address varchar(255),
+    savedOrder varchar(255),
+    lastOrder varchar(255)
+);
+CREATE TABLE food 
+(
+    foodID int,
+    menuCategory varchar(255),
+    name varchar(255),
+    price decimal(4,2),
+    onMenuNow boolean
+);
+CREATE TABLE pastOrders 
+(
+  orderID int,
+  userID int,
+  employeeID int,
+  itemsOrdered varchar(255),
+  subtotal decimal(4,2),
+  total decimal(4,2),
+  startTime decimal(4,2),
+  CompletionTime decimal(4,2)
+);
+CREATE TABLE ingredients 
+(
+    ingredientID int,
+    name varchar(255),
+    menuCategory varchar(255),
+    customizationStyle varchar(255)
+);
+CREATE TABLE employee
+(
+    employeeID int,
+    firstName varchar(255),
+    lastName varchar(255),
+    password varchar(255)
+);
+INSERT INTO food VALUES (0, "salad", "salad", 2.89, true);
+INSERT INTO food VALUES (1, "pizza", "cheese", 4.89, true);
+INSERT INTO food VALUES (2, "pizza", "pepperoni", 5.49, true);
+INSERT INTO food VALUES (3, "pizza", "bbq_chicken", 6.89, true);
+INSERT INTO food VALUES (4, "pizza", "meat_lovers", 6.89, true);
+INSERT INTO food VALUES (5, "pizza", "hawaiian", 6.89, true);
+INSERT INTO food VALUES (6, "pizza", "margherita", 5.49, true);
+INSERT INTO food VALUES (7, "pizza", "margheritin", 5.49, true);
+INSERT INTO food VALUES (8, "pizza", "vegan", 5.39, true);
+INSERT INTO food VALUES (9, "calzone", "cheese", 5.89, true);
+INSERT INTO food VALUES (10, "calzone", "pepperoni", 6.49, true);
+INSERT INTO food VALUES (11, "calzone", "bbq_chicken", 7.89, true);
+INSERT INTO food VALUES (12, "calzone", "meat_lovers", 7.89, true);
+INSERT INTO food VALUES (13, "calzone", "hawaiian", 7.89, true);
+INSERT INTO food VALUES (14, "calzone", "margherita", 6.49, true);
+INSERT INTO food VALUES (15, "calzone", "margheritin", 6.49, true);
+INSERT INTO food VALUES (16, "calzone", "vegan", 6.39, true);
+INSERT INTO food VALUES (17, "soup", "beef_bean_chili_8oz", 2.89, true);
+INSERT INTO food VALUES (18, "soup", "beef_bean_chili_12oz", 3.89, true);
+INSERT INTO food VALUES (19, "soup", "beef_bean_chili_12oz", 3.89, true);
+INSERT INTO food VALUES (20, "coffee", "coffee_12oz", 1.45, true);
+INSERT INTO food VALUES (21, "coffee", "coffee_16oz", 1.80, true);
+INSERT INTO food VALUES (22, "coffee", "coffee_20oz", 2.05, true);
+INSERT INTO food VALUES (23, "tea", "tea_12oz", 1.19, true);
+INSERT INTO food VALUES (24, "tea", "tea_16oz", 1.59, true);
+INSERT INTO food VALUES (25, "tea", "tea_20oz", 1.89, true);
+INSERT INTO food VALUES (26, "cider", "apple_12oz", 1.95, true);
+INSERT INTO food VALUES (27, "cider", "apple_16oz", 2.35, true);
+INSERT INTO food VALUES (28, "cider", "apple_20oz", 2.65, true);
+INSERT INTO food VALUES (29, "smoothie", "smoothie_16oz", 5.89, true);
+INSERT INTO food VALUES (30, "sandwich", "baker_bacon", 6.89, true);
+INSERT INTO food VALUES (31, "sandwich", "bbq_chicken", 6.89, true);
+INSERT INTO food VALUES (32, "sandwich", "classic_blt", 4.89, true);
+INSERT INTO food VALUES (33, "sandwich", "classic_italian", 6.89, true);
+INSERT INTO food VALUES (34, "sandwich", "darly_divine", 6.89, true);
+INSERT INTO food VALUES (35, "sandwich", "farrand_fodder", 6.89, true);
+INSERT INTO food VALUES (36, "sandwich", "hallett_ham", 6.89, true);
+INSERT INTO food VALUES (37, "sandwich", "libby_licious", 6.89, true);
+INSERT INTO food VALUES (38, "sandwich", "parisian_grilled_cheese", 5.89, true);
+INSERT INTO food VALUES (39, "sandwich", "roast_beef_pepper_melt", 7.89, true);
+INSERT INTO food VALUES (40, "sandwich", "tofu_mozzarella", 5.89, true);
+INSERT INTO food VALUES (41, "sandwich", "tuna_melt", 5.89, true);
+INSERT INTO food VALUES (42, "sandwich", "turkey_bacon_club", 6.89, true);
+INSERT INTO food VALUES (43, "sandwich", "turkey_jalapeno_melt", 6.89, true);
+INSERT INTO food VALUES (44, "sandwich", "veggie_club", 6.89, true);
+INSERT INTO food VALUES (45, "gluten_free", "hallett_ham_sandwich", 6.89, true);
+INSERT INTO food VALUES (46, "gluten_free", "veggie_club_sandwich", 6.89, true);
+INSERT INTO food VALUES (47, "gluten_free", "roast_turkey_wrap", 6.89, true);
+INSERT INTO food VALUES (48, "gluten_free", "heavenly_hummus_wrap", 6.89, true);
+INSERT INTO food VALUES (49, "condiment", "ranch", 0, true);
+INSERT INTO food VALUES (50, "condiment", "spicy_ranch", 0, true);
+INSERT INTO food VALUES (51, "condiment", "mustard", 0, true);
+INSERT INTO food VALUES (52, "condiment", "brown_mustard", 0, true);
+INSERT INTO food VALUES (53, "condiment", "mayo", 0, true);
+INSERT INTO food VALUES (54, "condiment", "bbq", 0, true);
+INSERT INTO food VALUES (55, "condiment", "hot_sauce", 0, true);
+INSERT INTO food VALUES (56, "condiment", "pesto", 0, true);
+INSERT INTO food VALUES (57, "condiment", "hummus", 0, true);
+INSERT INTO food VALUES (58, "condiment", "honey_mustard", 0, true);
+INSERT INTO food VALUES (59, "condiment", "italian", 0, true);
+INSERT INTO food VALUES (60, "condiment", "avocado", 0, true);
+INSERT INTO food VALUES (61, "bread_choice", "white_baguette", 0, true);
+INSERT INTO food VALUES (62, "bread_choice", "wheat_baguette", 0, true);
+INSERT INTO food VALUES (63, "bread_choice", "white_ciabatta", 0, true);
+INSERT INTO food VALUES (64, "bread_choice", "potato_rosemary_ciabatta", 0, true);
+INSERT INTO food VALUES (65, "bread_choice", "focaccia", 0, true);
+INSERT INTO food VALUES (66, "bread_choice", "7_grain", 0, true);
+INSERT INTO food VALUES (67, "dessert", "gelato_8oz", 2.89, true);
+INSERT INTO food VALUES (68, "dessert", "gelato_12oz", 3.69, true);
+INSERT INTO food VALUES (69, "dessert", "sundae_8oz", 2.89, true);
+INSERT INTO food VALUES (70, "dessert", "sundae_12oz", 3.69, true);
+INSERT INTO food VALUES (71, "dessert", "milkshake", 5.89, true);
+INSERT INTO food VALUES (72, "dessert", "avalanche", 5.89, true);
+INSERT INTO food VALUES (73, "dessert", "hot_chocolate_12oz", 2.85, true);
+INSERT INTO food VALUES (73, "dessert", "hot_chocolate_16oz", 3.55, true);
+INSERT INTO food VALUES (74, "dessert", "hot_chocolate_20oz", 3.85, true);
+INSERT INTO food VALUES (75, "greens", "field_greens", 0, true);
+INSERT INTO food VALUES (76, "greens", "romaine", 0, true);
+INSERT INTO food VALUES (77, "greens", "spinach", 0, true);
+INSERT INTO food VALUES (78, "veggies", "tomato", 0, true);
+INSERT INTO food VALUES (79, "veggies", "onion", 0, true);
+INSERT INTO food VALUES (80, "veggies", "olives", 0, true);
+INSERT INTO food VALUES (81, "veggies", "mushrooms", 0, true);
+INSERT INTO food VALUES (82, "veggies", "bell_peppers", 0, true);
+INSERT INTO food VALUES (83, "veggies", "banana_peppers", 0, true);
+INSERT INTO food VALUES (84, "veggies", "broccoli", 0, true);
+INSERT INTO food VALUES (85, "veggies", "cucumber", 0, true);
+INSERT INTO food VALUES (86, "veggies", "jalapeno", 0, true);
+INSERT INTO food VALUES (87, "veggies", "corn", 0, true);
+INSERT INTO food VALUES (88, "veggies", "garbanzo_beans", 0, true);
+INSERT INTO food VALUES (89, "veggies", "carrots", 0, true);
+INSERT INTO food VALUES (90, "cheese", "cheddar", 0, true);
+INSERT INTO food VALUES (91, "cheese", "feta", 0, true);
+INSERT INTO food VALUES (92, "cheese", "parmesan", 0, true);
+INSERT INTO food VALUES (93, "cheese", "blue", 0, true);
+INSERT INTO food VALUES (94, "cheese", "mozzarella", 0, true);
+INSERT INTO food VALUES (95, "protein", "turkey", 0, true);
+INSERT INTO food VALUES (96, "protein", "chicken", 0, true);
+INSERT INTO food VALUES (97, "protein", "tofu", 0, true);
+INSERT INTO food VALUES (98, "protein", "bacon", 0, true);
+INSERT INTO food VALUES (99, "protein", "tuna", 0, true);
+INSERT INTO food VALUES (100, "protein", "salami", 0, true);
+INSERT INTO food VALUES (101, "protein", "pepperoni", 0, true);
+INSERT INTO food VALUES (102, "protein", "roast_beef", 0, true);
+INSERT INTO food VALUES (103, "protein", "ham", 0, true);
+INSERT INTO food VALUES (104, "toppings", "croutons", 0, true);
+INSERT INTO food VALUES (105, "toppings", "cranberry_raisins", 0, true);
+INSERT INTO food VALUES (106, "toppings", "sesame_seeds", 0, true);
+INSERT INTO food VALUES (107, "dressing", "ranch", 0, true);
+INSERT INTO food VALUES (108, "dressing", "spicy_ranch", 0, true);
+INSERT INTO food VALUES (109, "dressing", "parmesan_peppercorn", 0, true);
+INSERT INTO food VALUES (110, "dressing", "red_wine_vinaigrette", 0, true);
+INSERT INTO food VALUES (111, "dressing", "raspberry_vinaigrette", 0, true);
+INSERT INTO food VALUES (112, "dressing", "balsamic_vinaigrette", 0, true);
+INSERT INTO food VALUES (113, "dressing", "caesar", 0, true);
+INSERT INTO food VALUES (114, "dressing", "italian", 0, true);
+INSERT INTO food VALUES (115, "dressing", "blue_cheese", 0, true);
+INSERT INTO food VALUES (116, "dressing", "honey_mustard", 0, true);
+INSERT INTO food VALUES (117, "dressing", "balsamic_vinegar_and_oil", 0, true);
