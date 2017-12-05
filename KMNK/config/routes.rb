51411326fd5af0c1d7785182d74ca18e6c713e81
Kira <kira.johnson@ymail.com>
2017-12-05 16:39:38 -0700

    More signup/login stuff - login page working???

--- KMNK/config/routes.rb
+++ KMNK/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout' 
+
   resources :users
   get 'order_items/create'
 
